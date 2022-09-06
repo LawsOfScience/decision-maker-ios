6ea995d55bc2125e2031ba3d60cc2adf31280b13
LawsOfScience <thesandwichsupreme@gmail.com>
2022-09-05 22:11:56 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,16 @@
+// ContentView.swift created by LawsOfScience (Aerasto) on 9/5/22
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Decision_MakerApp.swift
@@ -0,0 +1,12 @@
+// Decision_MakerApp.swift created by LawsOfScience (Aerasto) on 9/5/22
+
+import SwiftUI
+
+@main
+struct Decision_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
